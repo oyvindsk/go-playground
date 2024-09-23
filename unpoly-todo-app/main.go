687078f62aa11e29d28024eb6d0ce2f53a5928bc
Øyvind G. Skaar <os@oyvindsk.com>
2024-09-23 12:29:13 +0200

    unpoly+cursor-ai ++

--- unpoly-todo-app/main.go
+++ unpoly-todo-app/main.go
@@ -1,15 +1,25 @@
 package main
 
 import (
-	"net/http"
-
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
 	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
-	})
+	e.GET("/", indexHandler)
+	e.GET("/a", aHandler)
+	e.GET("/b", bHandler)
 	e.Logger.Fatal(e.Start(":8080"))
+}
+
+func indexHandler(c echo.Context) error {
+	return c.File("index.html")
+}
+
+func aHandler(c echo.Context) error {
+	return c.File("a.html")
+}
+
+func bHandler(c echo.Context) error {
+	return c.File("b.html")
 }
